05d63b35e1498dc42a1d1882f705e9bddc93e8b5
Brolocuelo <luisalfonsorello@gmail.com>
2022-12-12 10:45:48 +0100

    Avances

---
+++ Assets/Scripts/Exercise 1.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercise1 : MonoBehaviour
+{
+    public int totalMoney;
+
+    private void Update()
+    {
+        MoneyBox();
+    }
+
+    private void MoneyBox()
+    {
+        //if (Input.GetMouseButtonDown() += totalMoney)
+        {
+
+        }
+    }
+}
